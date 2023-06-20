36ed6806eb6885baee193e4c226678afc260c596
Essam Afzal <essam.afzal@outlook.com>
2023-06-20 14:33:24 +0400

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// STYLES
+import styles from "@/public/styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.main_heading}>Hello world</h1>
+    </main>
+  );
+}
