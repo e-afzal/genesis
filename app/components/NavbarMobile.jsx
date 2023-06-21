cd612c64a399f318650d08e5cd5f6563e5cd1208
Essam Afzal <essam.afzal@outlook.com>
2023-06-22 00:09:01 +0400

    Initiated Mobile Navbar

--- app/components/NavbarMobile.jsx
+++ app/components/NavbarMobile.jsx
@@ -1,6 +1,30 @@
+import Image from "next/image";
+
+// STYLES
+import styles from "@/public/styles/navbar_mobile.module.scss";
+
+// ASSET IMPORT
+import logo from "@/public/images/logo-white.svg";
+import menu from "@/public/icons/menu.svg";
+
 const NavbarMobile = () => {
   return (
-    <div>NavbarMobile</div>
+    <header id={styles.header_mobile}>
+      <nav className={styles.nav_mobile}>
+        <Image
+          src={logo}
+          alt="Genesis Brand Logo"
+          className={styles.nav_mobile_logo}
+          width={84}
+        />
+        <Image
+          src={menu}
+          alt="Navbar Hamburger Icon"
+          className={styles.nav_mobile_menu_icon}
+        />
+
+      </nav>
+    </header>
   );
 };
 
