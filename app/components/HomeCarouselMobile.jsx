5d6b526ef1e11bfa0009ec772defba6d90cdcee0
Essam Afzal <essam.afzal@outlook.com>
2023-06-26 18:16:27 +0400

    Home Carousel for Desktop & Mobile added

---
+++ app/components/HomeCarouselMobile.jsx
@@ -0,0 +1,59 @@
+"use client";
+//? SHOWCASE CAROUSEL
+import Link from 'next/link';
+import Image from "next/image";
+
+// SWIPER
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper";
+
+// STYLES
+import styles from "@/public/styles/home.module.scss";
+
+// ASSET IMPORT
+import chevron from "@/public/icons/chevron-down-white.svg";
+
+const HomeCarouselMobile = () => {
+  const carouselModels = [
+    { title: "genesis g70", caption: "raw force. unleashed.", url: "#" },
+    { title: "genesis g80", caption: "creating tomorrow, today.", url: "#" },
+    { title: "genesis g90", caption: "unrivalled elegance", url: "#" },
+    { title: "genesis gv70", caption: "equipped to perform. designed to inspire.", url: "#" },
+    { title: "genesis gv80", caption: "luxury without limits", url: "#" },
+  ];
+
+  return (
+    <Swiper
+      slidesPerView={"1"}
+      centeredSlides
+      className={styles.swiper_container_mobile}
+      modules={[Pagination, Autoplay]}
+      pagination={{ clickable: true }}
+    // autoplay={{ delay: 5000 }}
+    >
+      {carouselModels.map((each, index) => (
+        <SwiperSlide key={index} className={styles.swiper_slide_mobile}>
+          <div className={styles.swiper_content}>
+            {/* <h5 className={styles.model_year}>2023</h5> */}
+            <h2 className={styles.model_title}>{each.title}</h2>
+            <h4 className={styles.model_caption}>{each.caption}</h4>
+            <Link href={each.url} className={styles.discover_box}>
+              <span className={styles.link_text}>discover more
+              </span>
+              <Image
+                src={chevron}
+                alt="Chevron icon"
+                className={styles.model_link_chevron}
+              />
+            </Link>
+          </div>
+        </SwiperSlide>
+      ))}
+
+    </Swiper>
+  );
+};
+
+export default HomeCarouselMobile;
--- app/page.jsx
+++ app/page.jsx
@@ -7,13 +7,17 @@ import styles from "@/public/styles/home.module.scss";
 // COMPONENTS
 import Navbar from "./components/Navbar";
 import NavbarMobile from "./components/NavbarMobile";
+import HomeCarousel from './components/HomeCarousel';
+import HomeCarouselMobile from "./components/HomeCarouselMobile";
 import Footer from "./components/Footer";
 
 // ASSET IMPORT
-import g70 from "@/public/images/homepage/showcase_carousel/showcase_g70.jpg";
 import g90 from "@/public/images/homepage/featured_models/G90.jpg";
 import GV80 from "@/public/images/homepage/featured_models/GV80.jpg";
 import G80e from "@/public/images/homepage/featured_models/G80e.jpg";
+import benefits from "@/public/images/homepage/facets/owner_benefits.jpg";
+import runway from "@/public/images/homepage/facets/G90_runway.jpg";
+import gt from "@/public/images/homepage/facets/genesis_gt.jpg";
 
 export default function Home() {
   return (
@@ -24,11 +28,8 @@ export default function Home() {
 
         {/* SHOWCASE CAROUSEL */}
         <section id={styles.showcase}>
-          <Image
-            src={g70}
-            alt="G70"
-            width={1366}
-          />
+          <HomeCarousel />
+          <HomeCarouselMobile />
         </section>
 
         {/* OUR MODELS */}
@@ -37,7 +38,7 @@ export default function Home() {
           <p className={styles.models_description}>Explore Genesis vehicle line-up with its dynamic yet elegant design and cutting-edge technology</p>
         </section>
 
-        {/* FEATURED GRID */}
+        {/* DONE-FEATURED GRID */}
         <section id={styles.featured_models}>
           <h2 className={styles.discover_title}>discover the genesis collection</h2>
 
@@ -46,7 +47,7 @@ export default function Home() {
             <div className={styles.featured_card}>
               <Image
                 src={g90}
-                alt="G90 side view"
+                alt="The all-new 2023 Genesis G90 sedan"
               />
               <h3 className={styles.featured_model_title}>G90</h3>
               <p className={styles.featured_model_description}>
@@ -57,7 +58,7 @@ export default function Home() {
             <div className={styles.featured_card}>
               <Image
                 src={GV80}
-                alt="GV80 side view"
+                alt="The 2024 Genesis GV80 family-friendly SUV"
               />
               <h3 className={styles.featured_model_title}>GV80</h3>
               <p className={styles.featured_model_description}>Blending the refinement of our luxury sedans with enhanced versatility and cargo capacity, the GV80 adapts to all of life’s adventures.
@@ -68,7 +69,7 @@ export default function Home() {
             <div className={styles.featured_card}>
               <Image
                 src={G80e}
-                alt="G80e side view"
+                alt="The 2023 Genesis Electrified G80"
               />
               <h3 className={styles.featured_model_title}>electrified g80</h3>
               <p className={styles.featured_model_description}>Defining the next generation of electrification, Genesis leads with forward facing technologies and intriguing design, embodied in the Electrified G80.</p>
@@ -81,13 +82,55 @@ export default function Home() {
         <section id={styles.featured_model}></section>
 
         {/* VIDEO - GV70 */}
-        <section id={styles.featured_video}></section>
+        <section id={styles.featured_video}>
+          <iframe
+            src="https://www.youtube.com/embed/Tk5fvajw1RQ"
+            title="YouTube video player"
+            frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; web-share" allowFullScreen></iframe>
+        </section>
 
         {/* ELECTRIFY EVERY DRIVE */}
         <section id={styles.electrification}></section>
 
-        {/* FACETS OF GENESIS */}
-        <section id={styles.facets}></section>
+        {/* DONE-FACETS OF GENESIS */}
+        <section id={styles.facets}>
+          <h2 className={styles.facets_title}>experience the many facets of genesis</h2>
+
+          <div className={styles.facets_grid}>
+
+            <div className={styles.facets_card}>
+              <Image
+                src={benefits}
+                alt="Close up of the 2023 Genesis Electrified GV70"
+              />
+              <h3 className={styles.facets_model_title}>luxury that puts you first</h3>
+              <p className={styles.facets_model_description}>
+                Genesis vehicles are covered by industry-disrupting warranties and exceptional ownership benefits designed to enhance not only the way you drive, but the way you live.</p>
+              <Link href={"#"} className={styles.facets_model_link}>explore owner benefits</Link>
+            </div>
+
+            <div className={styles.facets_card}>
+              <Image
+                src={runway}
+                alt="Front view of the 2023 Genesis G90 two-line headlamps"
+              />
+              <h3 className={styles.facets_model_title}>from the highway to the runway</h3>
+              <p className={styles.facets_model_description}>A benchmark of refinement, the G90’s style-defining features are catching the eye of fashion’s elite, launching collaborations between Genesis and Vogue, NYFW, Monse, and more.
+              </p>
+              <Link href={"#"} className={styles.facets_model_link}>view the g90 design</Link>
+            </div>
+
+            <div className={styles.facets_card}>
+              <Image
+                src={gt}
+                alt="The Genesis G70 GR4 concept vehicle on the track"
+              />
+              <h3 className={styles.facets_model_title}>genesis races into gran turismo</h3>
+              <p className={styles.facets_model_description}>Push the G70 GR4 concept to top speeds or practice drifting with unprecedented realism as Genesis takes to the track as an Official Manufacturer Partner of the GT World Series.</p>
+              <Link href={"#"} className={styles.facets_model_link}>go inside the real genesis g70</Link>
+            </div>
+          </div>
+        </section>
 
       </main>
       <Footer />
