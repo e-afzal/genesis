0ccdef5097c41a25476118930c1d095c590c93db
e-afzal <essam.afzal@outlook.com>
2024-06-25 18:36:04 +0400

    Added OG Image

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,18 +5,20 @@ import "./globals.css";
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Genesis",
-  description: "Genesis automobiles",
+  title: "Genesis Motor",
+  description: "Genesis Motor Concept based project",
   openGraph: {
-    title: "Genesis",
-    description: "Genesis automobiles",
+    title: "Genesis Motor",
+    description: "Genesis Motor Concept based project",
     url: "https://genesis-ead.vercel.app",
     type: "website",
     images: [
       {
-        url: "https://www.essamafzal.com/thumbnail.png",
-        width: 1366,
-        height: 768,
+        url: "https://res.cloudinary.com/ddeima5fc/image/upload/v1719324267/genesis_thumbnail_i1mgaq.jpg",
+        width: 1920,
+        height: 780,
+        // width: 1200,
+        // height: 627,
       },
     ],
   },
@@ -33,7 +35,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
